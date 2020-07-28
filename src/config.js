9f0bd8096bf48bd76160a935f6abc590f75918d3
Seamus Le <seamust.le@gmail.com>
2020-07-27 20:00:12 -0700

    changed baseURL to live site

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://lol-finder-back.herokuapp.com';
-export const baseUrl = 'http://localhost:8080';
+export const baseUrl = 'https://lol-finder-back.herokuapp.com';
+// export const baseUrl = 'http://localhost:8080';
